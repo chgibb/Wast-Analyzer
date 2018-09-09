ea14e43f76108854f6729d604d63be0e2f14fda5
Chris Gibb <chris.gibb@outlook.com>
2018-09-09 15:25:07 -0400

    refactored test

--- __tests__/addInc.test.ts
+++ __tests__/addInc.test.ts
@@ -4,7 +4,7 @@ import * as fs from "fs";
 
 import {disassembleWat} from "./../lib/disassemble";
 import {parseSections} from "./../lib/parseSections";
-import {PrimitiveTypes, linkFunctionTypesToFunctions, linkTypeIndexesToFunctions,linkFunctionBodiesToFunctions} from "../lib/functionEntry";
+import {PrimitiveTypes, linkFunctionTypesToFunctions, FunctionSpace,linkFunctionBodiesToFunctions} from "../lib/functionEntry";
 
 it(`should parse sections`,() => {
     let dump = disassembleWat(fs.readFileSync("__tests__/addInc.wat").toString());
@@ -16,11 +16,11 @@ it(`should parse sections`,() => {
     expect(res.exportSection.contents.length).toBe(8);
     expect(res.nameSection.contents.length).toBe(32);
 
-  /*  let numImports = res.importSection.getNumberOfImports();
-    expect(numImports).toBe(0);
-
-    let functions = res.functionSection.getFunctionsWithTypeIndexes(numImports);
-    expect(functions.length).toBe(2);
+    let functions : FunctionSpace = new FunctionSpace(
+        res.functionSection.initializeInternalFunctionSpace(),
+        res.importSection.initializeImportedFunctionSpace()
+    );
+    expect(functions.internal.length).toBe(2);
 
     let types = res.typeSection.findFunctionTypes();
     expect(types.length).toBe(2);
@@ -32,24 +32,24 @@ it(`should parse sections`,() => {
     expect(types[1].parameters).toEqual([PrimitiveTypes.i32]);
     expect(types[1].result).toEqual(PrimitiveTypes.i32);
 
-    linkFunctionTypesToFunctions(functions,types);
+    linkFunctionTypesToFunctions(functions.internal,types);
     
-    res.nameSection.nameFunctions(functions,numImports);
+    res.nameSection.nameFunctions(functions);
 
-    expect(functions[0].name).toBe("add");
-    expect(functions[0].functionIndex).toBe(0);
-    expect(functions[0].type!.parameters).toEqual([PrimitiveTypes.i32,PrimitiveTypes.i32]);
-    expect(functions[0].type!.result).toBe(PrimitiveTypes.i32);
+    expect(functions.internal[0].name).toBe("add");
+    expect(functions.internal[0].functionIndex).toBe(0);
+    expect(functions.internal[0].type!.parameters).toEqual([PrimitiveTypes.i32,PrimitiveTypes.i32]);
+    expect(functions.internal[0].type!.result).toBe(PrimitiveTypes.i32);
 
-    expect(functions[1].name).toBe("inc");
-    expect(functions[1].functionIndex).toBe(1);
-    expect(functions[1].type!.parameters).toEqual([PrimitiveTypes.i32]);
-    expect(functions[1].type!.result).toBe(PrimitiveTypes.i32);
+    expect(functions.internal[1].name).toBe("inc");
+    expect(functions.internal[1].functionIndex).toBe(1);
+    expect(functions.internal[1].type!.parameters).toEqual([PrimitiveTypes.i32]);
+    expect(functions.internal[1].type!.result).toBe(PrimitiveTypes.i32);
 
     let bodies = res.codeSection.getFunctionBodies();
-    linkFunctionBodiesToFunctions(functions,bodies,numImports);
+    linkFunctionBodiesToFunctions(functions,bodies);
 
-    expect(functions[0].body).toEqual(
+    expect(functions.internal[0].body).toEqual(
 `0000027: 00                                        ; func body size (guess)
 0000028: 00                                        ; local decl count
 0000029: 20                                        ; get_local
@@ -59,7 +59,7 @@ it(`should parse sections`,() => {
 000002d: 6a                                        ; i32.add
 000002e: 0b                                        ; end
 0000027: 07                                        ; FIXUP func body size`.split(/\n/));
-    expect(functions[1].body).toEqual(
+    expect(functions.internal[1].body).toEqual(
 `000002f: 00                                        ; func body size (guess)
 0000030: 00                                        ; local decl count
 0000031: 20                                        ; get_local
@@ -69,6 +69,6 @@ it(`should parse sections`,() => {
 0000035: 6a                                        ; i32.add
 0000036: 0b                                        ; end
 000002f: 07                                        ; FIXUP func body size
-0000025: 11                                        ; FIXUP section size`.split(/\n/));*/
+0000025: 11                                        ; FIXUP section size`.split(/\n/));
 
 });
